8eee224231509732b04fe34b981f9c44da89a80b
alex-rus <aleksey.bash@gmail.com>
2022-11-06 13:23:20 +0300

    Выполненное задание №3: Сделать всё то же самое, но для контактов

---
+++ contact.py
@@ -0,0 +1,8 @@
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, nickname, email):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.email = email
--- test_add_contact.py
+++ test_add_contact.py
@@ -3,8 +3,9 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from contact import Contact
 
-class TestAddGroup(unittest.TestCase):
+class TestAddContact(unittest.TestCase):
     def setUp(self):
         self.wdcontact = webdriver.Firefox()
         self.wdcontact.implicitly_wait(30)
@@ -12,8 +13,8 @@ class TestAddGroup(unittest.TestCase):
     def test_add_contact(self):
         wdcontact = self.wdcontact
         self.open_homepage(wdcontact)
-        self.login(wdcontact)
-        self.contact_creation(wdcontact)
+        self.login(wdcontact, username="admin", password="secret")
+        self.contact_creation(wdcontact, Contact(first_name="FN1", middle_name="MN1", last_name="LN1", nickname="NN1",email="EM1"))
         self.return_homepage(wdcontact)
         self.logout(wdcontact)
 
@@ -23,31 +24,31 @@ class TestAddGroup(unittest.TestCase):
     def return_homepage(self, wdcontact):
         wdcontact.find_element_by_link_text("home page").click()
 
-    def contact_creation(self, wdcontact):
+    def contact_creation(self, wdcontact, Contact):
         # Init contact creation
         wdcontact.find_element_by_link_text("add new").click()
         # Fullfill contact form
         wdcontact.find_element_by_name("firstname").click()
         wdcontact.find_element_by_name("firstname").clear()
-        wdcontact.find_element_by_name("firstname").send_keys("First_name4")
+        wdcontact.find_element_by_name("firstname").send_keys(Contact.first_name)
         wdcontact.find_element_by_name("middlename").clear()
-        wdcontact.find_element_by_name("middlename").send_keys("Middle_name4")
+        wdcontact.find_element_by_name("middlename").send_keys(Contact.middle_name)
         wdcontact.find_element_by_name("lastname").clear()
-        wdcontact.find_element_by_name("lastname").send_keys("Last_name5")
+        wdcontact.find_element_by_name("lastname").send_keys(Contact.last_name)
         wdcontact.find_element_by_name("nickname").clear()
-        wdcontact.find_element_by_name("nickname").send_keys("NIckname6")
+        wdcontact.find_element_by_name("nickname").send_keys(Contact.nickname)
         wdcontact.find_element_by_name("email").click()
         wdcontact.find_element_by_name("email").clear()
-        wdcontact.find_element_by_name("email").send_keys("email6")
+        wdcontact.find_element_by_name("email").send_keys(Contact.email)
         # Submit contact creation
         wdcontact.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def login(self, wdcontact):
+    def login(self, wdcontact, username, password):
         wdcontact.find_element_by_name("user").click()
         wdcontact.find_element_by_name("user").clear()
-        wdcontact.find_element_by_name("user").send_keys("admin")
+        wdcontact.find_element_by_name("user").send_keys(username)
         wdcontact.find_element_by_name("pass").clear()
-        wdcontact.find_element_by_name("pass").send_keys("secret")
+        wdcontact.find_element_by_name("pass").send_keys(password)
         wdcontact.find_element_by_id("LoginForm").submit()
 
     def open_homepage(self, wdcontact):
